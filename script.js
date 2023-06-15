2363bc03af46ac1f3f4b83cba02aba5a4247e1d9
MustapheMS <moztaphem9@gmail.com>
2023-06-15 21:38:36 +0300

    Reverse Name Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+
+//Done 💯💯
+
+function reverseWords(sentence) {
+  let words = sentence.split(" ");
+  words.reverse();
+  let reversedSentence = words.join(" ");
+  return reversedSentence;
+}
+console.log(reverseWords("fikrcamp bootcamp"));
+console.log(reverseWords("hello there!")); 
+console.log(reverseWords("welcome to my city")); 
+console.log(reverseWords("Hargeisa of University"))
